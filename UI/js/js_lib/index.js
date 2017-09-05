d806d0f2660cd8fd4060e68dd3c98bf2a8bcf2ec
saman <samanf74@gmail.com>
2017-09-05 10:39:31 +0430

    just a lil forwarding in user info

--- UI/js/js_lib/index.js
+++ UI/js/js_lib/index.js
@@ -13,3 +13,38 @@ function get_user_info(userData) {
         }
     )
 }
+
+
+function load_user(userData, success) {
+    var url = "/Thesis/api/index.php"
+    $.post(
+        url, userData, function (data, status) {
+            console.log(data)
+            console.log(typeof data)
+            if (typeof data == "string") {
+                data = JSON.parse(data)
+            }
+            success(data)
+        }
+    )
+}
+
+
+window.onload = function () {
+    var chart1 = document.getElementById("line-chart").getContext("2d");
+    window.myLine = new Chart(chart1).Line(lineChartData, {
+        responsive: true,
+        scaleLineColor: "rgba(0,0,0,.2)",
+        scaleGridLineColor: "rgba(0,0,0,.05)",
+        scaleFontColor: "#c5c7cc"
+    });
+
+//        get_user_info({email: "samanf74@gmail.com"})
+
+    load_user({email: "samanf74@gmail.com"}, load_user_data_in_page)
+};
+
+function load_user_data_in_page(userData){
+    $("#profile_img").attr('src', userData['image'])
+    $("#profile_username").html(userData['name'])
+}
---
+++ api/index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SKings
+ * Date: 9/5/2017
+ * Time: 9:51 AM
+ */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "thesis";
+
+$result = (object) array();
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare('SELECT * FROM users WHERE email = :email');
+
+    $email = $_POST['email'];
+
+    $stmt->execute(array('email' => $email));
+
+    $stmt = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    echo json_encode($stmt);
+//    if(sizeof($stmt) > 0){
+//        $myfile = fopen($stmt['info_url'], "r+") or die("Unable to open file!");
+//        echo fread($myfile,filesize($stmt['info_url']));
+//        fclose($myfile);
+//    } else{
+//
+//    }
+//    foreach ($stmt as $row) {
+//        $myfile = fopen($row->info_url, "r+") or die("Unable to open file!");
+//        echo fread($myfile,filesize($row->info_url));
+//        fclose($myfile);
+//    }
+
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+$conn = null;
